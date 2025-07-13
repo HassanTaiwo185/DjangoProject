b0685a5d87e08bbe19185a15267597b6121aee46
Hassan Ayinde <ayindehassan776@gmail.com>
2025-07-13 17:16:24 -0400

    custom user model created and set up media files in the settings to support images . custom user model also added in the the settings as our auth user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import  AbstractBaseUser , PermissionsMixin 
+import uuid
+from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
+class CustomBaseUserManager(BaseUserManager):
+    def _create_user(self,username,password,**extra_fields):
+        if not username:
+            raise ValueError("Username is required")
+        
+        user = self.model(username=username,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    def create_user(self,username=None,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',False)
+        extra_fields.setdefault('is_superuser',False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomBaseUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
