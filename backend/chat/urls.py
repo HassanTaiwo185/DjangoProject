08f6a78d1c663239a1fa0876eac962b68b71da52
HassanTaiwo185 <ayindehassan776@gmail.com>
2025-07-24 16:38:50 -0400

    listmessages url is now added

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -4,6 +4,7 @@ from .views import RoomViews, MessageViews , ListMessagesByStandup , EditMessage
 urlpatterns = [
     path("rooms/", RoomViews.as_view(), name="room-list-create"),
     path("messages/", MessageViews.as_view(), name="message-list-create"),
+    path("messages/standup/<uuid:standup_id>/", ListMessagesByStandup.as_view(), name="messages-by-standup"),
     path("messages/standup/<int:pk>/", EditMessageView.as_view(), name="edit-message"),
     path("messages/standup/<int:pk>/", DeleteMessageView.as_view(), name="delete-message"),
 ]
